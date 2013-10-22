b00830caf2c65c687c385c3a2f1327f2f2a15ab2
eroneko <coop.blue.atlas@gmail.com>
2013-10-22 13:24:49 +0800

    Carbon changes

--- carbon.mk
+++ carbon.mk
@@ -1,22 +1,17 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := mione_plus
 
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common.mk)
-
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 # old audio files
 include frameworks/base/data/sounds/OldAudio.mk
 
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Ring_Digital_02.ogg \
-    ro.config.notification_sound=F1_New_SMS.ogg \
-    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 PRODUCT_PACKAGES += \
     HoloSpiralWallpaper \
@@ -35,7 +30,7 @@ $(call inherit-product, device/xiaomi/mione_plus/device_mione_plus.mk)
 
 # Setup device specific product configuration.
 PRODUCT_DEVICE := mione_plus
-PRODUCT_NAME := cm_mione_plus
+PRODUCT_NAME := carbon_mione_plus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI-ONE Plus
 PRODUCT_MANUFACTURER := Xiaomi
@@ -47,3 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=mione_plus \
 	BUILD_FINGERPRINT="Xiaomi/mione_plus/mione_plus:4.1.2/JZO54K/QDS84:userdebug/test-keys" \
 	PRIVATE_BUILD_DESC="mione_plus-userdebug 4.1.2 JZO54K QDS84 test-keys"
+
